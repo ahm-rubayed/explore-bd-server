9e64311e5016e7bcb2c4d204a2752fec2d134c07
Maruf Hossain <marufrony48@gmail.com>
2023-02-20 02:54:08 +0600

    g

--- index.js
+++ index.js
@@ -84,6 +84,13 @@ async function run() {
       res.send(users);
     });
 
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    });
+
     app.get("/users/admin/:email", async (req, res) => {
       const email = req.params.email;
       const query = { email };
